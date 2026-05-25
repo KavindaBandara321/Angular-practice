b83f8868dc5f7d106d32ae558de53fb8494fa96a
Kavinda Bandara <kavindabandara94@gmail.com>
2026-05-25 12:55:17 +0530

    Signals added

--- src/user/user.component.ts
+++ src/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -12,21 +12,17 @@ const randomUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 
 
-
-
-
-
-
-
-
-
-
-
 export class UserComponent {
 
-  selectedUser = DUMMY_USERS[randomUserIndex];
+  selectedUser = signal(DUMMY_USERS[randomUserIndex]);
 
 get imageLink(){
-  return 'assets/users/'+ this.selectedUser.avatar;
+  return 'assets/users/'+ this.selectedUser().avatar;
 }
+
+onSelectUser(){
+  const randomUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  this.selectedUser.set(DUMMY_USERS[randomUserIndex]);
+}
+
 }
